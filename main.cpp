9c40d9db479c20702fc9f4bb728a82c06a471bc9
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-17 02:28:10 +0900

    複数の文字列を定義

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world." << endl;
+    // 三つの文字列を定義する
+    string str1 = "Hello";
+    string str2 = "world";
+    string str3 = str1 + " " + str2 + "!";
+    cout << str3 << endl;
     return 0;
 }
