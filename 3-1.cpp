e328fba6e4dd0deb31ec10329fed405398ec9987
YukaChoco <sugar.ore.yk@gmail.com>
2024-04-30 15:48:25 +0900

    コマンドライン引数追加

--- 3-1.cpp
+++ 3-1.cpp
@@ -3,15 +3,26 @@
 
 // 定数の指定
 const int MAX = 50;           // 最大反復回数
-const double X0 = 1.;         // 初期値
 const double EPS_FX = 1.0e-6; // 終了条件の誤差
 
 // 関数f(x),df(x)の定義
 inline double f(double x) { return (x - cos(x)); }
 inline double df(double x) { return (1 + sin(x)); }
 
-int main()
+// 解
+const double ANSWER = 0.73908513;
+
+int main(int argc, char *argv[])
 {
+    // コマンドライン引数の表示
+    if (argc != 2)
+    {
+        std::cout << "コマンドライン引数で適切に初期値が指定されませんでした." << std::endl;
+        return 1;
+    }
+
+    // コマンドライン引数の初期値x_0の取得
+    double X0 = atof(argv[1]);
     // 変数xを初期値x_0で初期化
     double x = X0;
     // 更新量delta_x,関数値の誤差fx_errの定義
@@ -23,8 +34,12 @@ int main()
     // 出力ストリーム std::cout の精度を設定するコードです。
     // std::cout を使用して浮動小数点数を出力する際に、小数点以下の桁数を指定
 
+    // 計算条件の表示
+    std::cout << "\n[計算条件]" << std::endl;
     // 終了条件に用いる誤差の表示
-    std::cout << "\n終了条件に用いる誤差: " << EPS_FX << "\n"
+    std::cout << "終了条件に用いる誤差: " << EPS_FX << std::endl;
+    // 初期値x0の表示
+    std::cout << "初期値x0: " << X0 << "\n"
               << std::endl;
 
     // 初期値x0の表示
@@ -50,7 +65,9 @@ int main()
     }
 
     // 終了時の結果の表示
-    std::cout << "\n結果: x = " << x << "\n"
+    std::cout << "\n結果: x = " << x << std::endl;
+    // 誤差の表示
+    std::cout << "解との誤差: " << std::fixed << x - ANSWER << "\n"
               << std::endl;
 
     return 0;
